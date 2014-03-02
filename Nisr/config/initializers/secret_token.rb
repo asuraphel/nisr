2480ce7dce0bab1716d41c707f88aedba0c9bed6
Alexander <asuraphel@apposit.com>
2014-03-02 11:01:51 +0300

    Just after first bundle

---
+++ Nisr/config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Nisr::Application.config.secret_key_base = '8dae64dfa1bfe7701e554539aac766ff03f76bf0570fa8031fcfd3f23824f840cbc9c77f60d69de5262ab809bd3d12f76614c05327487e257ec62953ecc8cc6a'
---
+++ Nisr/config/initializers/session_store.rb
@@ -0,0 +1,3 @@
+# Be sure to restart your server when you modify this file.
+
+Nisr::Application.config.session_store :cookie_store, key: '_Nisr_session'
