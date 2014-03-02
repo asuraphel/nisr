5e74297e313c7e9f10466ab9f4d5f9fa6df19367
Alexander <asuraphel@apposit.com>
2014-03-02 21:10:49 +0300

    Added options to survey creation page

--- Nisr/app/controllers/surveys_controller.rb
+++ Nisr/app/controllers/surveys_controller.rb
@@ -69,6 +69,6 @@ class SurveysController < ApplicationController
 
     # Never trust parameters from the scary internet, only allow the white list through.
     def survey_params
-      params.require(:survey).permit(:name)
+      params.require(:survey).permit(:name, :stype_id)
     end
 end
